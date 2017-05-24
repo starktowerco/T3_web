7d7e7a0327ca66f5cbc811666f4a8c28e682b7a4
kylejohnson2612 <kylejohnson2612@gmail.com>
2017-05-24 16:05:29 -0600

    Feat/react home page (#2)
    * (Feat) Will add react and react strap to the homepage.
    
    * (Fix) Rename assets folder
    
    * (Fix) Revert README.md
    
    * (Feat) update favicon.io
    
    * (Fix) Update short name and name

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+  Row,
+  Col,
+  Jumbotron,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from 'reactstrap';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="inverse" inverse toggleable>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand href="/">T3</NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <Jumbotron>
+          <Container>
+            <Row>
+              <Col xs="4"></Col>
+              <Col xs="4">
+                <h1>Welcome to T3</h1>
+                <p>
+                  <Button
+                    tag="a"
+                    color="success"
+                    size="large"
+                    href="#"
+                    block
+                  >
+                    Login to Play
+                  </Button>
+                </p>
+              </Col>
+              <Col xs="4"></Col>
+            </Row>
+          </Container>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+export default App;
